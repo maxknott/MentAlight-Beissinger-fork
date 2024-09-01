cdae307b1678699853b8f3ebb98fa11f9db41c1c
maxknott <max.knott1@web.de>
2024-09-02 01:04:39 +0200

    initial reward
    initial Constructors, Getters etc. for Reward and RewardManager

--- app/src/main/java/com/example/mentalight/Reward.java
+++ app/src/main/java/com/example/mentalight/Reward.java
@@ -3,7 +3,42 @@ package com.example.mentalight;
 //Klasse für einen Reward Screen
 public class Reward {
 
+    private String title;
+    private String subtitle;
+    private String text;
+    private Badge badge;
 
+
+    //Konstruktor für Reward Screen mit Badge
+    public Reward(String title, String subtitle, String text, Badge badge) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.text = text;
+        this.badge = badge;
+    }
+
+    //Konstruktor für Reward Screen ohne Badge
+    public Reward(String title, String subtitle, String text) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
 
 
 }
--- app/src/main/java/com/example/mentalight/RewardManager.java
+++ app/src/main/java/com/example/mentalight/RewardManager.java
@@ -2,6 +2,48 @@ package com.example.mentalight;
 
 public class RewardManager {
 
+    private Reward reward;
+    private Reward rewardScreen;
+    private Badge badge;
 
+    private String title;
+    private String subtitle;
+    private String text;
+
+    public RewardManager() {
+        init();
+    }
+
+    private void init() {
+        //TODO: string constants for title, subtitle, text
+        //title =
+        //subtitle =
+        //text =
+        makeRewardScreen();
+    }
+
+    private void makeRewardScreen() {
+        if(true){
+            rewardScreen = rewardWithBadge();
+        } else {
+            rewardScreen = rewardWithoutBadge();
+        }
+    }
+
+    private Reward rewardWithBadge() {
+        reward = new Reward(title, subtitle, text, badge);
+        return reward;
+    }
+
+    private Reward rewardWithoutBadge() {
+        reward = new Reward(title, subtitle, text);
+        return reward;
+    }
+
+
+    //returns complete reward screen
+    public Reward getRewardScreen() {
+        return rewardScreen;
+    }
 
 }
